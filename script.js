2be6a6ca34a0eb549cd809813e1e81b72bd2b082
Darryl Ansah <darrylansah4856@gmail.com>
2020-08-10 15:47:27 -0400

    monday

---
+++ script.js
@@ -0,0 +1,35 @@
+let api = "https://api.data.charitynavigator.org/v2/Organizations?app_id=fdc3d132&app_key=dbbd7d630df788b5c498c1a97cfb7e87";
+
+fetch(api)
+    .then(function (response) {
+        return response.json();
+
+
+    })
+    .then(function (myJson) {
+        printInfo(myJson);
+
+    });
+
+function printInfo(info) {
+
+    console.log(info);
+
+
+}
+
+let api2 = "https://gnews.io/api/v3/search?q=example&token=3069c2ba43fc4972906cf4e38414e2f7";
+
+fetch(api2)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (myJson) {
+        printNews(myJson);
+    });
+
+function printNews(news) {
+    console.log(news);
+}
+
+
